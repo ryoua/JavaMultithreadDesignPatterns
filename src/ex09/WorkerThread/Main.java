9c7debbcc11b3c71b2b4eb74c49fa897c43a20cc
徐炳华 <31928820+ryoua@users.noreply.github.com>
2019-03-01 12:20:38 +0800

    WorkerThread

--- src/ex09/WorkerThread/Main.java
+++ src/ex09/WorkerThread/Main.java
@@ -1,4 +1,11 @@
 package ex09.WorkerThread;
 
 public class Main {
+    public static void main(String[] args) {
+        Channel channel = new Channel(5);
+        channel.startWorkers();
+        new ClientThread("Alice", channel).start();
+        new ClientThread("Bobby", channel).start();
+        new ClientThread("Chris", channel).start();
+    }
 }
