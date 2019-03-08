f0f9f9634b3fba2fb9e784426794121395c39a2c
徐炳华 <31928820+ryoua@users.noreply.github.com>
2019-03-08 15:05:06 +0800

    ThreadPerMessage

--- src/ex08/ThreadPerMessage/Host.java
+++ src/ex08/ThreadPerMessage/Host.java
@@ -1,15 +1,27 @@
 package ex08.ThreadPerMessage;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
 public class Host {
     private final Helper helper = new Helper();
+    private final Executor executor;
+
+    public Host(Executor executor) {
+        this.executor= executor;
+    }
 
     public void request(final int count, final char c) {
         System.out.println("   request(" + count + ", " + c + ") BEGIN");
-        new Thread() {
-            public void run() {
-                helper.handle(count, c);
-            }
-        }.start();
+        executor.execute(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        helper.handle(count, c);
+                    }
+                }
+        );
         System.out.println("   request(" + count + ", " + c + ") END");
     }
 }
--- src/ex08/ThreadPerMessage/Main.java
+++ src/ex08/ThreadPerMessage/Main.java
@@ -1,12 +1,27 @@
 package ex08.ThreadPerMessage;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Thread-Per-Message模式
+ * 角色: Client(委托人), Host, Helper(助手)
+ * 范围: 提高响应性, 缩短延迟时间, 适用于操作顺序没有要求时, 适用于不需要返回值时, 适用于服务器
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("main BEGIN");
-        Host host = new Host();
-        host.request(10, 'A');
-        host.request(20, 'B');
-        host.request(30, 'C');
-        System.out.println("main END");
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Host host = new Host(
+                executorService
+        );
+        try {
+            host.request(10, 'A');
+            host.request(20, 'B');
+            host.request(30, 'C');
+        } finally {
+            executorService.shutdown();
+            System.out.println("main END");
+        }
     }
 }
