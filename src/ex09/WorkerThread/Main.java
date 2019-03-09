29d8e381642725cfb51d687392a6f56a247cd614
徐炳华 <31928820+ryoua@users.noreply.github.com>
2019-03-09 21:47:47 +0800

    WorkerThread

--- src/ex09/WorkerThread/Main.java
+++ src/ex09/WorkerThread/Main.java
@@ -1,5 +1,8 @@
 package ex09.WorkerThread;
 
+/**
+ * WorkerThread 模式// ThreadPool线程池
+ */
 public class Main {
     public static void main(String[] args) {
         Channel channel = new Channel(5);
